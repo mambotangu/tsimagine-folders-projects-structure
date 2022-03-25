dc848f026a73d1730fa4c3b173552c2fa7e3becd
Stacy Véronneau <stacy.veronneau@sada.com>
2022-03-25 10:58:12 -0400

    making sure .terrform and archived are not pkg.

--- package_for_customer.sh
+++ package_for_customer.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 #
-[[ ! -f sada-foundation.tgz ]] || rm sada-foundation.tgz
+[[ ! -f ../sada-foundation.tgz ]] || rm -f ../sada-foundation.tgz
 [[ -d sada-foundation ]] || mkdir sada-foundation
+mkdir sada-foundation/1-bootstrap
+mkdir sada-foundation/2-organization
+mkdir sada-foundation/3-shared
+mkdir sada-foundation/4-dev
+mkdir sada-foundation/5-qa
+mkdir sada-foundation/6-uat
+mkdir sada-foundation/7-prod
+mkdir sada-foundation/modules
 #
 echo
 echo Preparing......
 echo
-cp -R 1-bootstrap ./sada-foundation/
-cp -R 2-organization ./sada-foundation/
-cp -R 3-shared ./sada-foundation/
-cp -R 4-dev  ./sada-foundation/
-cp -R 5-qa ./sada-foundation/
-cp -R 6-uat ./sada-foundation/
-cp -R 7-prod ./sada-foundation/
+cd 1-bootstrap ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/1-bootstrap
+cd ../2-organization ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/2-organization
+cd ../3-shared ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/3-shared
+cd ../4-dev ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/4-dev
+cd ../5-qa ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/5-qa
+cd ../6-uat ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/6-uat
+cd ../7-prod ; cp -R `ls -A | grep -v ".terraform"` ../sada-foundation/7-prod
+cd ../modules ; cp -R `ls -A | grep -v "archived"` ../sada-foundation/modules
+cd ..
 cp -R csr ./sada-foundation/
 mkdir ./sada-foundation/docs
 mkdir ./sada-foundation/docs/img
 cp ./docs/getting_started_fast.md ./sada-foundation/docs/
 cp -r ./docs/img/* ./sada-foundation/docs/img/
-cp -R modules ./sada-foundation/
 cp .gitignore ./sada-foundation/
 cp 0-prep.sh ./sada-foundation/
 cp 0.5-prep.sh ./sada-foundation/
