012a347e7e36182bbf89c0efe2cef8b02efedb6e
Barclay <barclayiversen@gmail.com>
2022-07-27 19:55:36 -0700

    project updates

--- 0-prep/project.py
+++ 0-prep/project.py
@@ -1,10 +1,16 @@
+from venv import create
 from helpers import *
 import subprocess
 import shlex
+import os
 import json
 import difflib
 
 from random import randint
+
+
+def invoke_subprocess(command_str: str, capture_output: bool = False, text: bool = False):
+    return subprocess.run(command_str.split(" "), capture_output=capture_output, text=text)
 
 
 def check_for_project(org_id):
@@ -29,11 +35,13 @@ def create_workspace_project(org_id):
     create_prj_cmd = "gcloud projects create foundation-workspace-" + \
         str(suffix) + " --name=foundation-workspace --organization=" + \
         org_id + " --labels=type=foundation"
-    create_prj_output = subprocess.check_output(
-        shlex.split(create_prj_cmd), shell=True)
-    print(create_prj_output)
-    # create_prj_json = json.loads(create_prj_output)
-    # print_colored(create_prj_json, "red")
+    try:
+        create_prj_output = subprocess.check_output(
+            shlex.split(create_prj_cmd), shell=True)
+        print(create_prj_output)
+    except:
+        print_colored("Error creating project...", "red")
+        print(create_prj_output)
 
 
 def enable_admin(project_id):
@@ -44,9 +52,9 @@ def enable_admin(project_id):
             shlex.split(enable_api_cmd), shell=True)
         print_colored("Successfully enabled admin API.", "grn")
         enable_iam(project_id)
-    except:
+    except Exception as e:
         print_colored("There was an error:", "red")
-        print(enable_api_output)
+        print(e)
 
 
 def enable_iam(project_id):
@@ -56,6 +64,60 @@ def enable_iam(project_id):
         enable_iam_output = subprocess.check_output(
             shlex.split(enable_iam_cmd), shell=True)
         print_colored("Successfully enabled IAM API.", "grn")
+        check_for_service_account(project_id)
     except Exception as e:
-        print_colored("There was an error enabling IAM:" + e, "red")
-        print(enable_iam_output)
+        print_colored("There was an error enabling IAM:", "red")
+        print(e)
+
+
+def check_for_service_account(project_id):
+    print_colored(
+        "Checking for service account in foundation workspace project...", "prpl")
+    sa_list_cmd = "gcloud iam service-accounts list --format=json --project=" + project_id
+    try:
+        sa_list_output = subprocess.check_output(
+            shlex.split(sa_list_cmd), shell=True)
+        sa_list_json = json.loads(sa_list_output)
+        for sa in sa_list_json:
+            if(sa["email"] == "sa-admin-caller@" + project_id + ".iam.gserviceaccount.com"):
+                print_colored("Service account found", "grn")
+            check_for_key_file(project_id, sa["oauth2ClientId"])
+            return
+        print_colored(
+            "Admin caller SA not found", "prpl")
+        create_service_account(project_id)
+    except Exception as e:
+        print_colored("Error checking for service account", "red")
+        print(e)
+
+
+def create_service_account(project_id):
+    print_colored("Creating service account...", "prpl")
+    create_sa_cmd = "gcloud iam service-accounts create sa-admin-caller --description='For making workspace API calls' --display-name='workspace-admin-api-caller' --project=" + project_id
+    try:
+        sa_cmd_output = subprocess.check_output(
+            shlex.split(create_sa_cmd), shell=True)
+        print_colored("SA created. Getting oauth client ID", "grn")
+        check_for_service_account(project_id)
+    except Exception as e:
+        print_colored("Error creating service account...", "red")
+        print(e)
+
+
+def check_for_key_file(project_id, clientId):
+    print_colored("Checking for SA key...", "prpl")
+    try:
+        files_cmd_output = subprocess.check_output(
+            "ls", shell=True)
+        if(str(files_cmd_output).find("sa-admin-caller")):
+            print('Key found')
+        else:
+            print("No key found.")
+            create_sa_key(project_id)
+    except Exception as e:
+        print_colored("Error checking for key file", "red")
+        print(e)
+
+
+def create_sa_key(project_id):
+    print("creating key")
